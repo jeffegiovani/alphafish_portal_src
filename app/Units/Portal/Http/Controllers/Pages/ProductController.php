f8eea11ecf78357e5c0ad6bcc75325327f8471da
Jefferson Giovani <jeffe.giovani@gmail.com>
2022-11-30 11:24:26 -0300

    Clean code
    Remove tests and examples

--- app/Units/Portal/Http/Controllers/Pages/ProductController.php
+++ app/Units/Portal/Http/Controllers/Pages/ProductController.php
@@ -6,34 +6,6 @@ use PortalApp\Http\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
-
-    public function metanol()
-    {
-        $this->seo()->setTitle('Metanol');
-        $this->seo()->setDescription('Metanol');
-
-        return $this->view('pages.product.metanol');
-    }
-
-
-    public function nitrometano()
-    {
-        $this->seo()->setTitle('Nitrometano');
-        $this->seo()->setDescription('Nitrometano');
-
-        return $this->view('pages.product.nitrometano');
-    }
-
-
-    public function m5()
-    {
-        $this->seo()->setTitle('M5');
-        $this->seo()->setDescription('M5');
-
-        return $this->view('pages.product.m5');
-    }
-
-
     public function list()
     {
         $this->seo()->setTitle('Titulo da página');
